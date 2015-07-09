a073e8eb4b235efbaf8aacc3ed18f818925d7965
chapeter <chapeter@cisco.com>
2015-07-09 13:44:05 -0500

    ;lkj

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 service mysql start
 mysqladmin -u root password cisco
 python /opt/acitoolkit/applications/endpointtracker/aci-endpoint-tracker.py -u http://10.94.238.68 -l admin -p cisco123 -i 127.0.0.1 -a root -s cisco &
-python /opt/acitoolkit/applications/endpointtracker/aci-endpoint-tracker-gui.py -a root -s cisco
-python /opt/acitoolkit/applications/snapback.py
+python /opt/acitoolkit/applications/endpointtracker/aci-endpoint-tracker-gui.py -i 127.0.0.1 -a root -s cisco
+python /opt/acitoolkit/applications/snapback.py/snapback.py
 /bin/bash
